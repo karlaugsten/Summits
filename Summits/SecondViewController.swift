0f886553ef2bf12401dbccac6c9ad41e8eaa2190
Karl Augsten <karlaugsten@gmail.com>
2014-10-26 16:11:10 -0600

    Initial Commit

---
+++ Summits/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Summits
+//
+//  Created by Karl Augsten on 2014-10-26.
+//  Copyright (c) 2014 Summits LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
